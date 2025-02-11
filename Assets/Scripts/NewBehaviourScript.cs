1b59b77a2df28fdc1f7f367d8e412e45f94ada35
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-02-11 11:13:29 +0700

    feat(Plane) : Jalan Kiri dan kanan dan mengitu arah mouse

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public float speed = 5.0f;
+     public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Walk();
+        RotateTowardsMouse();
     }
+
+    public void Walk() {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+    }
+
+    public void RotateTowardsMouse() {
+        float angle = Mathf.Atan2(Input.mousePosition.y - transform.position.y, Input.mousePosition.x - transform.position.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
 }
